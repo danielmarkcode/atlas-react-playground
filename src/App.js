5952bb4c337170d255d1c28536d1a6c542ff64f8
Daniel Mark <danielmarkcode@gmail.com>
2022-08-12 13:39:27 +0530

    Fixed Standard Config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import { Steppers } from "@cnx-grp/common-ui-lib";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // PROP 1
+  const steps = [
+    {
+      // Stepper Labels
+      label: "Step 0",
+      // Stepper Section Click Handler
+      handleClick: () => {
+        if (activeStep > 0) {
+          setActiveStep(0);
+        }
+      },
+    },
+    {
+      label: "Step 1",
+      handleClick: () => {
+        if (activeStep > 1) {
+          setActiveStep(1);
+        }
+      },
+    },
+    {
+      label: "Step 2",
+      handleClick: () => {
+        if (activeStep > 2) {
+          setActiveStep(2);
+        }
+      },
+    },
+  ];
+
+  // PROP 2
+  // Pass Active Step
+  const [activeStep, setActiveStep] = React.useState(2);
+  return <Steppers steps={steps} activeStep={activeStep} />;
 }
 
 export default App;
